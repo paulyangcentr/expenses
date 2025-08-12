ad3ec163dfb29796252c8d6c47ed77bd8e9a1383
Paul Yang <paulyang@Mac.attlocal.net>
2025-08-11 21:03:41 -0700

    Update schema for production deployment with PostgreSQL support

---
+++ test-db.js
@@ -0,0 +1,1 @@
+const { PrismaClient } = require("@prisma/client"); const prisma = new PrismaClient(); prisma.$connect().then(() => { console.log("Connected to database"); prisma.$disconnect(); }).catch(console.error);
